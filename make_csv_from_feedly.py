1d07b82d22fe173f3b60cdfc3f7039000587f024
laixintao <glenn.lai@bosondata.com.cn>
2018-02-11 13:19:14 +0800

    init commit

---
+++ make_csv_from_feedly.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+import json
+import csv
+
+
+header = [
+    "标题",
+    "feed"
+    "网站主页",
+    "描述",
+    "feedly 订阅数",
+    "标签",
+    "更新频率"
+    "分类",
+]
+
+
+with open("feedly.json") as feedly_file:
+    data = json.loads(feedly_file.read())
+
+with open("feeds.csv", "w+") as feeds:
+     writer = csv.writer(feeds)
+     writer.writerow(header)
+     for item in data:
+         feed_url = item['id'][4:]
+         title = item['title']
+         subscribers = item['subscribers']
+         site = item['website']
+         description = ""
+         categorie = item['categories'][0]['label']
+         topics = item.get('topics')
+         weekly = "每周{}篇".format(item['velocity'])
+         writer.writerow([
+             title,
+             feed_url,
+             site,
+             description,
+             subscribers,
+             topics,
+             weekly,
+             categorie,
+        ])
